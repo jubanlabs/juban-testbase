4a46d4d885c9f01409f3eb8299a3f92801722119
Zefei Ji <3108252+zefeiji@users.noreply.github.com>
2022-10-04 15:01:50 +0800

    x

---
+++ src/Jubanlabs.TestBase/JubanTestBase.cs
@@ -0,0 +1,15 @@
+﻿namespace Jubanlabs.TestBase
+{
+    using Jubanlabs.Logging;
+    using Microsoft.Extensions.Hosting;
+    public class JubanTestBase
+    {
+        public JubanTestBase()
+        {
+            var host = Host.CreateDefaultBuilder()
+                .Build().SetupJubanLogger();
+
+            Console.WriteLine("JubanTestBase construction done");
+        }
+    }
+}
